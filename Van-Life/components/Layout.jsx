bfba4bcb5932ce23b6d604e049edd09ab5a088cb
Lesego Shange <makgeledisal@gmail.com>
2024-06-11 09:01:28 +0200

    Components

---
+++ Van-Life/components/Layout.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Layout() {
+  return (
+    <div className="site-wrapper">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
